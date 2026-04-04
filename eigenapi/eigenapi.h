e4e53681abcb813b9c26417d4fecca6abb4f9afc
TheTechnobear <mharris@technobear.com>
2026-04-04 18:38:11 +0200

    tidying up due to EH

--- eigenapi/eigenapi.h
+++ eigenapi/eigenapi.h
@@ -6,7 +6,7 @@
 
 namespace EigenApi {
 class Callback {
-   public:
+public:
     enum DeviceType {
         PICO,
         TAU,
@@ -15,20 +15,25 @@ class Callback {
     virtual ~Callback() = default;
 
     // information
-    virtual void beginDeviceInfo() {}
-    virtual void deviceInfo(bool isPico, unsigned devEnum, const char* dev) {}
-    virtual void endDeviceInfo() {}
+    virtual void beginDeviceInfo() { ; }
+    virtual void deviceInfo(bool /*isPico*/, unsigned /*devEnum*/, const char* /*dev*/) { ; }
+    virtual void endDeviceInfo() { ; }
 
     // device events
     // note: dev = usb id, name is a 'friendly' enumeration of usb devices
-    virtual void connected(const char* dev, DeviceType dt){};
-    virtual void disconnected(const char* dev){};
+    virtual void connected(const char* /*dev*/, DeviceType /*dt*/) { ; }
+    virtual void disconnected(const char* /*dev*/) { ; }
 
-    virtual void key(const char* dev, unsigned long long t, unsigned course, unsigned key, bool a, float p, float r, float y){};
-    virtual void breath(const char* dev, unsigned long long t, float val){};
-    virtual void strip(const char* dev, unsigned long long t, unsigned strip, float val, bool a){};
-    virtual void pedal(const char* dev, unsigned long long t, unsigned pedal, float val){};
-    virtual void dead(const char* dev, unsigned reason){};
+    virtual void key(const char* /*dev*/, unsigned long long /*t*/, unsigned /*course*/, unsigned /*key*/, bool /*a*/,
+                     float /*p*/, float /*r*/, float /*y*/) {
+        ;
+    }
+    virtual void breath(const char* /*dev*/, unsigned long long /*t*/, float /*val*/) { ; }
+    virtual void strip(const char* /*dev*/, unsigned long long /*t*/, unsigned /*strip*/, float /*val*/, bool /*a*/) {
+        ;
+    }
+    virtual void pedal(const char* /*dev*/, unsigned long long /*t*/, unsigned /*pedal*/, float /*val*/) { ; }
+    virtual void dead(const char* /*dev*/, unsigned /*reason*/) { ; }
 };
 
 class IFW_Reader;
@@ -37,7 +42,7 @@ class IFW_Reader;
 // any pointers passed in, are owned by caller
 // they are assumed valid until explicitly removed, or api is destroyed
 class Eigenharp {
-   public:
+public:
     explicit Eigenharp();  // use an embedded reader
     explicit Eigenharp(IFW_Reader* fwReader);
     virtual ~Eigenharp();
@@ -64,16 +69,16 @@ class Eigenharp {
     void setPollTime(unsigned pollTime);
 
     // allBasePico 0= All, 1 = basestation only, 2 = pico only
-    // devEnum 0= All, 1-N = device 
+    // devEnum 0= All, 1-N = device
     void setDeviceFilter(unsigned allBasePico, unsigned devEnum);
 
-   private:
+private:
     void* impl;
 };
 
 // Firmware reader
 class IFW_Reader {
-   public:
+public:
     virtual ~IFW_Reader() = default;
     virtual bool open(const char* filename, int oFlags, void** fd) = 0;
     virtual long read(void* fd, void* data, long byteCount) = 0;
@@ -83,7 +88,7 @@ class IFW_Reader {
 
 // firmware reader implementation for OSX/Linux files
 class FWR_Posix : public EigenApi::IFW_Reader {
-   public:
+public:
     explicit FWR_Posix(const char* path);
 
     bool open(const char* filename, int oFlags, void** fd) override;
@@ -91,32 +96,32 @@ class FWR_Posix : public EigenApi::IFW_Reader {
     void close(void* fd) override;
     bool confirmResources() override;
 
-   private:
+private:
     const char* path_;
 };
 
 // use firmware embedeed into EigenLite
 class FWR_Embedded : public EigenApi::IFW_Reader {
-   public:
+public:
     explicit FWR_Embedded();
-    virtual ~FWR_Embedded();
+    virtual ~FWR_Embedded() override;
 
     bool open(const char* deviceihx, int oFlags, void** fd) override;
     long read(void* fd, void* data, long byteCount) override;
     void close(void* fd) override;
     bool confirmResources() override;
 
-   private:
+private:
     long position_ = 0;
     unsigned maxLen_ = 0;
 };
 
 class Logger {
-   public:
+public:
     static void setLogFunc(void (*pLogFn)(const char*));
     static void logmsg(const char*);
 
-   private:
+private:
     static void (*_logmsg)(const char*);
 };
 }  // namespace EigenApi
